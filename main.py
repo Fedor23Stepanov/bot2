fc87b26af35396dfdd7ab7ca0072423d2276ff63
Fedor23Stepanov <128918076+Fedor23Stepanov@users.noreply.github.com>
2025-05-13 17:26:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ from config import TELEGRAM_TOKEN
 from db import init_db
 from handlers import register_handlers
 from tasks import start_scheduler
+import logging
+
+# Настройка базового логирования
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,            # или DEBUG для более подробных логов
+)
+
+# Уровень логирования для модуля PTB
+logging.getLogger("telegram").setLevel(logging.INFO)
+logging.getLogger("telegram.bot").setLevel(logging.INFO)
+logging.getLogger("apscheduler").setLevel(logging.INFO)
+
+# Если хотите видеть сами SQL-запросы:
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
 async def main():
     # 1) Инициализация БД и устройств
